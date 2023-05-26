4a516062539b09beabea9a4d6dad8971856b5303
Gustavo <106829052+GustavoKehr@users.noreply.github.com>
2023-05-26 12:53:23 -0300

    iniciando curso de python

---
+++ main2.py
@@ -0,0 +1,5 @@
+diaNasc = input('Qual seu dia de nascimento?')
+mesNasc = input('Qual o seu mes de nascimento?')
+anoNasc = input('Qual seu ano de nascimento')
+
+print('O dia em que nasceu foi', diaNasc, 'no mes', mesNasc,  'no ano de', anoNasc)
---
+++ main3.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite mais um número: '))
+soma = n1 + n2
+
+print('A soma dos valores foi de:', soma)
