acb52c071cf0d4d7e13ea402461ce18071947acf
Gustavo <106829052+GustavoKehr@users.noreply.github.com>
2023-12-06 22:50:47 -0300

    add

---
+++ M3/CEV-Python-Exercicios/ex072().py
@@ -0,0 +1,11 @@
+# Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
+# Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
+numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze',
+           'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+while True:
+    num = int(input('Digite um número [0-20]: '))
+    if 0 <= num <= 20:
+        break
+print(f'Numero escolhido {num}\nNumero por extenso {numeros[num]}')
